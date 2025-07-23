e2da7f14d0005d7a7e6a952b290c538f96f776b6
Dhinesh Kaviraaj <dhineshkaviraaj@gmail.com>
2025-07-23 14:47:28 +0530

    Time: 1551 ms (53.71%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,21 +1,5 @@
-WITH CTE AS (
-SELECT 
-COUNT( CASE WHEN income < 20000 THEN 1 END ) AS low_salary,
-COUNT( CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END ) AS average_salary,
-COUNT( CASE WHEN income > 50000 THEN 1 END ) AS high_salary
-FROM Accounts
-)
-
-SELECT 'High Salary' AS category,                              -- converting it to vertical column
-        high_salary  AS accounts_count FROM CTE
-UNION ALL
-SELECT 'Low Salary', low_salary FROM CTE
-UNION ALL
-SELECT 'Average Salary',  average_salary FROM CTE
-
--- Here basically, 
--- we choose 2 things, 'High Salary' and high_salary and name them as category and accounts_count
--- we then fit stuff in that by selecting and using union
-
-
--- NOTE: betweeen is inclusive
+SELECT 'High Salary' as category, COUNT(account_id) AS 'accounts_count' FROM Accounts WHERE income>50000
+UNION
+SELECT 'Low Salary' as category, COUNT(account_id) AS 'accounts_count' FROM Accounts WHERE income<20000
+UNION
+SELECT 'Average Salary' as category, COUNT(account_id) AS 'average_count' FROM Accounts WHERE income BETWEEN '20000' AND '50000'
